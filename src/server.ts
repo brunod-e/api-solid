3398cf65f66401c837cc1b7585f3d8ffe260b888
Bruno <brunodaniel.elias@gmail.com>
2023-12-19 17:43:21 -0300

    feat: configure import alias

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import { app } from "./app"
-import { env } from "./env"
+import { env } from "@/env"
 
 app
-	.listen({
-		host: "0.0.0.0",
-		port: env.PORT,
-	})
-	.then((address) => {
-		console.log(`🔥 Server running on port ${env.PORT} in ${env.NODE_ENV} mode`)
-	})
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`🔥 Server running on port ${env.PORT} in ${env.NODE_ENV} mode`)
+  })
