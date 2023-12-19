6a434861155ba26c09717e1d68c8c113d3721c76
Bruno <brunodaniel.elias@gmail.com>
2023-12-19 17:35:10 -0300

    feat: add eslint config

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,14 +2,14 @@ import "dotenv/config"
 import { z } from "zod"
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["dev", "prod", "test"]).default("dev"),
-  PORT: z.coerce.number().default(3333),
+	NODE_ENV: z.enum(["dev", "prod", "test"]).default("dev"),
+	PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
-  throw new Error(`❌ Invalid env: ${_env.error}`)
+	throw new Error(`❌ Invalid env: ${_env.error}`)
 }
 
 export const env = _env.data
--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,10 @@ import { app } from "./app"
 import { env } from "./env"
 
 app
-  .listen({
-    host: "0.0.0.0",
-    port: env.PORT,
-  })
-  .then((address) => {
-    console.log(`🔥 Server running on port ${env.PORT} in ${env.NODE_ENV} mode`)
-  })
+	.listen({
+		host: "0.0.0.0",
+		port: env.PORT,
+	})
+	.then((address) => {
+		console.log(`🔥 Server running on port ${env.PORT} in ${env.NODE_ENV} mode`)
+	})
