7f803f9cdc02f5cc1b76f140f0d2ebaf32859b29
Bruno <brunodaniel.elias@gmail.com>
2024-01-27 16:35:53 -0300

    add: repository pattern

---
+++ src/repositories/in-memory-users-repository.ts
@@ -0,0 +1,8 @@
+import { Prisma } from "@prisma/client";
+
+export class InMemoryUsersRepository {
+  public users: Prisma.UserCreateInput[] = [];
+  async create(data: Prisma.UserCreateInput) {
+    this.users.push(data);
+  }
+}
--- src/services/register.ts
+++ src/services/register.ts
@@ -1,34 +1,35 @@
-import { prisma } from "@/lib/prisma"
-import { hash } from "bcryptjs"
+import { prisma } from "@/lib/prisma";
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
+import { hash } from "bcryptjs";
 
 interface RegisterProps {
-    name: string
-    email: string
-    password: string
+  name: string;
+  email: string;
+  password: string;
 }
 
 export async function registerService({
+  name,
+  email,
+  password,
+}: RegisterProps) {
+  const password_hash = await hash(password, 6);
+
+  const userAlreadyExists = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userAlreadyExists) {
+    throw new Error("User already exists");
+  }
+
+  const prismaUsersRepository = new PrismaUsersRepository();
+
+  await prismaUsersRepository.create({
     name,
     email,
-    password
-}: RegisterProps) {
-    const password_hash = await hash(password, 6)
-
-    const userAlreadyExists = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
-  
-    if (userAlreadyExists) {
-      throw new Error("User already exists")
-    }
-  
-    await prisma.user.create({
-      data: {
-        name,
-        email,
-        password_hash,
-      },
-    })
+    password_hash,
+  });
 }
