9cb57be52fba21af0f0a4b582a85dc2101fbe396
Bruno <brunodaniel.elias@gmail.com>
2024-01-27 16:19:25 -0300

    add: register service

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,38 +1,21 @@
-import { FastifyRequest, FastifyReply } from "fastify"
-import { hash } from "bcryptjs"
-import { prisma } from "@/lib/prisma"
-import { z } from "zod"
+import { registerService } from "@/services/register";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
 
 export const register = async (req: FastifyRequest, reply: FastifyReply) => {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
     password: z.string().min(6),
-  })
+  });
 
-  const { name, email, password } = registerBodySchema.parse(req.body)
+  const { name, email, password } = registerBodySchema.parse(req.body);
 
-  const password_hash = await hash(password, 6)
-
-  const userAlreadyExists = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userAlreadyExists) {
-    return reply.status(409).send({
-      error: "User already exists",
-    })
+  try {
+    await registerService({ name, email, password });
+  } catch (error) {
+    reply.status(409).send();
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  })
-
-  return reply.status(201).send()
-}
+  return reply.status(201).send();
+};
