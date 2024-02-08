e86b15c7d40cca1a8191f3448844c4f74f8ac2e5
Bruno <brunodaniel.elias@gmail.com>
2024-02-08 16:14:19 -0300

    add: date validation at checkins

--- src/services/check-in.spec.ts
+++ src/services/check-in.spec.ts
@@ -2,14 +2,27 @@ import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { CheckInService } from "./check-in";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { Decimal } from "@prisma/client/runtime/library";
 
 let checkInsRepository: InMemoryCheckInsRepository;
+let gymsRepository: InMemoryGymsRepository;
 let sut: CheckInService;
 
 describe("Get User Profile Service", () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository();
-    sut = new CheckInService(checkInsRepository);
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CheckInService(checkInsRepository, gymsRepository);
+
+    gymsRepository.items.push({
+      id: "gym-id",
+      title: "Gym",
+      description: "Gym description",
+      phone: "",
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    });
 
     vi.useFakeTimers();
   });
@@ -22,6 +35,8 @@ describe("Get User Profile Service", () => {
     const { checkIn } = await sut.execute({
       userId: "user-id",
       gymId: "gym-id",
+      userLatitute: 0,
+      userLongitude: 0,
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
@@ -33,12 +48,16 @@ describe("Get User Profile Service", () => {
     await sut.execute({
       userId: "user-id",
       gymId: "gym-id",
+      userLatitute: 0,
+      userLongitude: 0,
     });
 
     await expect(() =>
       sut.execute({
         userId: "user-id",
         gymId: "gym-id",
+        userLatitute: 0,
+        userLongitude: 0,
       })
     ).rejects.toBeInstanceOf(Error);
   });
@@ -49,6 +68,8 @@ describe("Get User Profile Service", () => {
     await sut.execute({
       userId: "user-id",
       gymId: "gym-id",
+      userLatitute: 0,
+      userLongitude: 0,
     });
 
     vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0));
@@ -56,6 +77,8 @@ describe("Get User Profile Service", () => {
     const { checkIn } = await sut.execute({
       userId: "user-id",
       gymId: "gym-id",
+      userLatitute: 0,
+      userLongitude: 0,
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
--- src/services/check-in.ts
+++ src/services/check-in.ts
@@ -1,21 +1,36 @@
 import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { GymsRepository } from "@/repositories/gyms-repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
 interface CheckInServiceRequest {
   userId: string;
   gymId: string;
+  userLatitute: number;
+  userLongitude: number;
 }
 interface CheckInServiceResponse {
   checkIn: CheckIn;
 }
 
 export class CheckInService {
-  constructor(private checkInsRepository: CheckInsRepository) {}
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository
+  ) {}
 
   async execute({
     userId,
     gymId,
   }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
+    const gym = await this.gymsRepository.findById(gymId);
+
+    if (!gym) {
+      throw new ResourceNotFoundError();
+    }
+
+    // TODO: Calculate distance between user and gym
+
     const hasCheckInOnSameDate =
       await this.checkInsRepository.findByUserIdOnDate(userId, new Date());
 
