01df0187d0f79df6b3e5bb82e99ac44f39d61fe0
Bruno <brunodaniel.elias@gmail.com>
2024-04-09 17:01:35 -0300

    add: gyms controllers

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,23 @@
+import { makeFetchNearbyGymsService } from "@/services/factories/make-fetch-nearby-gyms-service";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+
+export const nearby = async (req: FastifyRequest, reply: FastifyReply) => {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine(value => Math.abs(value) <= 90),
+    longitude: z.number().refine(value => Math.abs(value) <= 180),
+  });
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(req.body);
+
+  const nearbyService = makeFetchNearbyGymsService();
+
+  const { gyms } = await nearbyService.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  });
+
+  return reply.status(201).send({
+    gyms,
+  });
+};
---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,23 @@
+import { makeSearchGymsService } from "@/services/factories/make-search-gyms-service";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+
+export const search = async (req: FastifyRequest, reply: FastifyReply) => {
+  const searchGymsQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  const { query, page } = searchGymsQuerySchema.parse(req.body);
+
+  const searchService = makeSearchGymsService();
+
+  const { gyms } = await searchService.execute({
+    query,
+    page,
+  });
+
+  return reply.status(201).send({
+    gyms,
+  });
+};
