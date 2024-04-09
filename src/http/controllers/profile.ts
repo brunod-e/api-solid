ada7a15b2279dc5485ec13cdffc4866c30531ca5
Bruno <brunodaniel.elias@gmail.com>
2024-04-09 11:54:22 -0300

    add: profile route logic

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,14 @@
+import { makeGetUserProfileService } from "@/services/factories/make-get-user-profile-service";
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export const profile = async (req: FastifyRequest, reply: FastifyReply) => {
-  await req.jwtVerify();
+  const getUserProfile = makeGetUserProfileService();
 
-  return reply.status(200).send();
+  const { user } = await getUserProfile.execute({ userId: req.user.sub });
+
+  Reflect.deleteProperty(user, "password_hash");
+
+  return reply.status(200).send({
+    user,
+  });
 };
